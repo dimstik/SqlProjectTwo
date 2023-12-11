9c7c36fdb3899b659f4e540b6867bcdea0a79e16
Dima Pavlov <dima.p96@mail.ru>
2023-12-11 17:18:26 +0300

    create class Rental

---
+++ src/main/java/org/example/entity/Rental.java
@@ -0,0 +1,88 @@
+package org.example.entity;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(schema = "movie", name = "rental")
+public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "rental_id")
+    private Integer id;
+    @Column(name = "rental_date")
+    private LocalDateTime rentalDate;
+    @ManyToOne
+    @JoinColumn(name = "inventory_id")
+    private Inventory inventory;
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+    @Column(name = "return_date")
+    private LocalDateTime returnDate;
+    @ManyToOne
+    @JoinColumn(name = "staff_id")
+    private Staff staff;
+    @Column(name = "last_update")
+    @UpdateTimestamp
+    private LocalDate lastUpdate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getRentalDate() {
+        return rentalDate;
+    }
+
+    public void setRentalDate(LocalDateTime rentalDate) {
+        this.rentalDate = rentalDate;
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public LocalDateTime getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDateTime returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public Staff getStaff() {
+        return staff;
+    }
+
+    public void setStaff(Staff staff) {
+        this.staff = staff;
+    }
+
+    public LocalDate getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalDate lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+}
